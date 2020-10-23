61df77415534fa26138d9da6d3bf2964332a2286
abugrayhat <abugrayhat@gmail.com>
2020-10-23 15:53:29 +0700

    menambahkan string cantik pada output

--- OOP1-NuryadinAbutani-TIFRP19A/src/Main.java
+++ OOP1-NuryadinAbutani-TIFRP19A/src/Main.java
@@ -15,16 +15,16 @@ class Main {
         
         //menu
         while(true){
-            System.out.println("=========================");
-            System.out.println("\tMENU GAME");
-            System.out.println("=========================");
+            System.out.println("========================================");
+            System.out.println("\t\tMENU GAME");
+            System.out.println("========================================");
             System.out.println("1. Input Game A");
             System.out.println("2. Input Game B");
             System.out.println("3. Input Game C");
             System.out.println("4. Input Game D");
             System.out.println("5. Lihat Data Game");
             System.out.println("6. Keluar");
-            System.out.println("=========================");    
+            System.out.println("========================================");    
             
             //input
             System.out.print("Masukan Pilihan: ");
@@ -35,14 +35,14 @@ class Main {
             switch(pilihan){
                 case 1:
                     String kode, nama, lisensi;
-                    System.out.println("=========================");
-                    System.out.println("\tINPUT GAME A");
-                    System.out.println("=========================");
-                    System.out.print("Masukan Kode\t: ");
+                    System.out.println("========================================");
+                    System.out.println("\t\tINPUT GAME A");
+                    System.out.println("========================================");
+                    System.out.print("Masukan Kode\t\t: ");
                     kode = br.readLine();
-                    System.out.print("Masukan Nama\t: ");
+                    System.out.print("Masukan Nama\t\t: ");
                     nama = br.readLine();
-                    System.out.print("Masukan Lisensi\t: ");
+                    System.out.print("Masukan Lisensi\t\t: ");
                     lisensi = br.readLine();
                     
                     gameA = new Game(kode, nama, lisensi);
@@ -51,18 +51,18 @@ class Main {
                     break;
                 case 2:
                     String jenis, tipe;
-                    System.out.println("=========================");
-                    System.out.println("\tINPUT GAME B");
-                    System.out.println("=========================");
-                    System.out.print("Masukan Kode\t: ");
+                    System.out.println("========================================");
+                    System.out.println("\t\tINPUT GAME B");
+                    System.out.println("========================================");
+                    System.out.print("Masukan Kode\t\t: ");
                     kode = br.readLine();
-                    System.out.print("Masukan Nama\t: ");
+                    System.out.print("Masukan Nama\t\t: ");
                     nama = br.readLine();
-                    System.out.print("Masukan Lisensi\t: ");
+                    System.out.print("Masukan Lisensi\t\t: ");
                     lisensi = br.readLine();
-                    System.out.print("Masukan Jenis\t: ");
+                    System.out.print("Masukan Jenis\t\t: ");
                     jenis = br.readLine();
-                    System.out.print("Masukan Tipe\t: ");
+                    System.out.print("Masukan Tipe\t\t: ");
                     tipe = br.readLine();
                     
                     gameB = new Game(kode, nama, lisensi,jenis, tipe);
@@ -71,18 +71,18 @@ class Main {
                     break;
                 case 3:
                     String ukuran;
-                    System.out.println("=========================");
-                    System.out.println("\tINPUT GAME C");
-                    System.out.println("=========================");
-                    System.out.print("Masukan Kode\t: ");
+                    System.out.println("========================================");
+                    System.out.println("\t\tINPUT GAME C");
+                    System.out.println("========================================");
+                    System.out.print("Masukan Kode\t\t: ");
                     kode = br.readLine();
-                    System.out.print("Masukan Nama\t: ");
+                    System.out.print("Masukan Nama\t\t: ");
                     nama = br.readLine();
-                    System.out.print("Masukan Lisensi\t: ");
+                    System.out.print("Masukan Lisensi\t\t: ");
                     lisensi = br.readLine();
-                    System.out.print("Masukan Jenis\t: ");
+                    System.out.print("Masukan Jenis\t\t: ");
                     jenis = br.readLine();
-                    System.out.print("Masukan Tipe\t: ");
+                    System.out.print("Masukan Tipe\t\t: ");
                     tipe = br.readLine();
                     System.out.print("Masukan Ukuran file\t: ");
                     ukuran = br.readLine();
@@ -93,18 +93,18 @@ class Main {
                     break;
                 case 4:
                     String rilis;
-                    System.out.println("=========================");
-                    System.out.println("\tINPUT GAME D");
-                    System.out.println("=========================");
-                    System.out.print("Masukan Kode\t: ");
+                    System.out.println("========================================");
+                    System.out.println("\t\tINPUT GAME D");
+                    System.out.println("========================================");
+                    System.out.print("Masukan Kode\t\t: ");
                     kode = br.readLine();
-                    System.out.print("Masukan Nama\t: ");
+                    System.out.print("Masukan Nama\t\t: ");
                     nama = br.readLine();
-                    System.out.print("Masukan Lisensi\t: ");
+                    System.out.print("Masukan Lisensi\t\t: ");
                     lisensi = br.readLine();
-                    System.out.print("Masukan Jenis\t: ");
+                    System.out.print("Masukan Jenis\t\t: ");
                     jenis = br.readLine();
-                    System.out.print("Masukan Tipe\t: ");
+                    System.out.print("Masukan Tipe\t\t: ");
                     tipe = br.readLine();
                     System.out.print("Masukan Ukuran file\t: ");
                     ukuran = br.readLine();
@@ -116,51 +116,51 @@ class Main {
                     System.out.println("*** Data Game D Berhasil disimpan ***");
                     break;
                 case 5:
-                    System.out.println("=========================");
-                    System.out.println("\tDATA GAME");
-                    System.out.println("=========================");
+                    System.out.println("========================================");
+                    System.out.println("\t\tDATA GAME");
+                    System.out.println("========================================");
                     
                     System.out.println("GAME A");
-                    System.out.println("Kode\t: "+gameA.getKode());
-                    System.out.println("Nama\t: "+gameA.getNama());
-                    System.out.println("Lisensi\t: "+gameA.getLisensi());
-                    System.out.println("Jenis\t: "+gameA.getJenis());
-                    System.out.println("Tipe\t: "+gameA.getTipe());
+                    System.out.println("Kode\t\t: "+gameA.getKode());
+                    System.out.println("Nama\t\t: "+gameA.getNama());
+                    System.out.println("Lisensi\t\t: "+gameA.getLisensi());
+                    System.out.println("Jenis\t\t: "+gameA.getJenis());
+                    System.out.println("Tipe\t\t: "+gameA.getTipe());
                     System.out.println("Ukuran File\t: "+gameA.getUkuran());
                     System.out.println("Tgl Rilis\t: "+gameA.getRilis());
                     
                     System.out.println("");
                     System.out.println("GAME B");
-                    System.out.println("Kode\t: "+gameB.getKode());
-                    System.out.println("Nama\t: "+gameB.getNama());
-                    System.out.println("Lisensi\t: "+gameB.getLisensi());
-                    System.out.println("Jenis\t: "+gameB.getJenis());
-                    System.out.println("Tipe\t: "+gameB.getTipe());
+                    System.out.println("Kode\t\t: "+gameB.getKode());
+                    System.out.println("Nama\t\t: "+gameB.getNama());
+                    System.out.println("Lisensi\t\t: "+gameB.getLisensi());
+                    System.out.println("Jenis\t\t: "+gameB.getJenis());
+                    System.out.println("Tipe\t\t: "+gameB.getTipe());
                     System.out.println("Ukuran File\t: "+gameB.getUkuran());
                     System.out.println("Tgl Rilis\t: "+gameB.getRilis());
                     
                     System.out.println("");
                     System.out.println("GAME C");
-                    System.out.println("Kode\t: "+gameC.getKode());
-                    System.out.println("Nama\t: "+gameC.getNama());
-                    System.out.println("Lisensi\t: "+gameC.getLisensi());
-                    System.out.println("Jenis\t: "+gameC.getJenis());
-                    System.out.println("Tipe\t: "+gameC.getTipe());
+                    System.out.println("Kode\t\t: "+gameC.getKode());
+                    System.out.println("Nama\t\t: "+gameC.getNama());
+                    System.out.println("Lisensi\t\t: "+gameC.getLisensi());
+                    System.out.println("Jenis\t\t: "+gameC.getJenis());
+                    System.out.println("Tipe\t\t: "+gameC.getTipe());
                     System.out.println("Ukuran File\t: "+gameC.getUkuran());
                     System.out.println("Tgl Rilis\t: "+gameC.getRilis());
                     
                     System.out.println("");
                     System.out.println("GAME D");
-                    System.out.println("Kode\t: "+gameD.getKode());
-                    System.out.println("Nama\t: "+gameD.getNama());
-                    System.out.println("Lisensi\t: "+gameD.getLisensi());
-                    System.out.println("Jenis\t: "+gameD.getJenis());
-                    System.out.println("Tipe\t: "+gameD.getTipe());
+                    System.out.println("Kode\t\t: "+gameD.getKode());
+                    System.out.println("Nama\t\t: "+gameD.getNama());
+                    System.out.println("Lisensi\t\t: "+gameD.getLisensi());
+                    System.out.println("Jenis\t\t: "+gameD.getJenis());
+                    System.out.println("Tipe\t\t: "+gameD.getTipe());
                     System.out.println("Ukuran File\t: "+gameD.getUkuran());
                     System.out.println("Tgl Rilis\t: "+gameD.getRilis());
                     break;
                 default:
-                    System.out.println("Perogram Keluar.");
+                    System.out.println("Perogram Telah Keluar.");
                     System.exit(0);
             }
         }
